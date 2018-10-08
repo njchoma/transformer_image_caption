e71255b1b6ce3e80f7f16acceff0ddc92b22ed81
Nicholas <nc2201@cs.nyu.edu>
2018-10-08 02:27:26 -0400

    Complete all remaining language layers with no beam search

--- src/caption_model.py
+++ src/caption_model.py
@@ -21,14 +21,18 @@ class Caption_Model(nn.Module):
         self.dict_size = dict_size
         self.image_feature_dim = image_feature_dim
 
-        self.embed_word = #TODO
+        self.embed_word = nn.Linear(dict_size, WORD_EMB_DIM, bias=False)
         self.lstm1 = Attention_LSTM(WORD_EMB_DIM,
                                     NB_HIDDEN_LSTM2,
                                     image_feature_dim,
                                     NB_HIDDEN_LSTM1)
-        self.lstm2 = #TODO
-        self.attention = #TODO
-        self.predict_word = #TODO
+        self.lstm2 = Language_LSTM(NB_HIDDEN_LSTM1,
+                                   NB_HIDDEN_ATT,
+                                   NB_HIDDEN_LSTM2)
+        self.attention = Visual_Attention(image_feature_dim,
+                                          NB_HIDDEN_LSTM1,
+                                          NB_HIDDEN_ATT)
+        self.predict_word = nn.Linear(NB_HIDDEN_LSTM2, dict_size)
     
     def forward(self, image_features, nb_timesteps):
         nb_batch, nb_image_feats, _ = image_features.size()
@@ -52,7 +56,7 @@ class Caption_Model(nn.Module):
 #####################################################
 class Attention_LSTM(nn.Module):
     def __init__(self, dim_word_emb, dim_lang_lstm, dim_image_feats, nb_hidden):
-        self.lstm_cell = nn.LSTMCell(dict_lang_lstm+dim_image_feats+dim_word_emb,
+        self.lstm_cell = nn.LSTMCell(dim_lang_lstm+dim_image_feats+dim_word_emb,
                                      nb_hidden,
                                      bias=True)
         
@@ -60,3 +64,52 @@ class Attention_LSTM(nn.Module):
         input_feats = torch.cat((h2, v_mean, word_emb),dim=1)
         h_out, c_out = self.lstm_cell(input_feats, h1, c1)
         return h_out, c_out
+
+class Language_LSTM(nn.Module):
+    def __init__(self, dim_att_lstm, dim_visual_att, nb_hidden):
+        self.lstm_cell = nn.LSTMCell(dim_att_lstm+dim_visual_att,
+                                     nb_hidden,
+                                     bias=True)
+        
+    def forward(self, h2, c2, h1, v_hat):
+        input_feats = torch.cat((h1, v_hat, word_emb),dim=1)
+        h_out, c_out = self.lstm_cell(input_feats, h2, c2)
+        return h_out, c_out
+
+
+class Visual_Attention(nn.Module):
+    def __init__(self, dim_image_feats, dim_att_lstm, nb_hidden):
+        self.fc_image_feats = nn.Linear(dim_image_feats, nb_hidden, bias=False)
+        self.fc_att_lstm = nn.Linear(dim_att_lstm, nb_hidden, bias=False)
+        self.act_tan = nn.Tanh()
+        self.fc_att = nn.Linear(nb_hidden, 1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, image_feats, h1):
+        nb_batch, nb_feats, feat_dim = image_feats.size()
+
+        att_lstm_emb = self.fc_att_lstm(h1).unsqueeze(1)
+        image_feats_emb = self.fc_image_feats(image_feats)
+        all_feats_emb = image_feats_emb + att_lstm_emb.repeat(1,nb_feats,1)
+
+        activate_feats = self.act_tan(all_feats_emb)
+        unnorm_attention = self.fc_att(activate_feats)
+        normed_attention = self.softmax(unnorm_feats).unsqueeze(2)
+
+        weighted_feats = normed_attention.repeat(1,1,nb_feats) * image_feats
+        attended_image_feats = weighted_feats.sum(dim=2)
+        return attended_image_feats
+
+class Predict_Word(nn.Module):
+    def __init__(self, dim_language_lstm, dict_size):
+        super(Predict_Word, self).__init__()
+        self.fc = nn.Linear(dim_language_lstm, dict_size):
+        
+    def forward(self, h2):
+        y = self.fc(h2)
+        return y, self._one_hot_word(y)
+
+    def _one_hot_word(self, y):
+        #TODO
+        # beam search??
+        return word_emb
