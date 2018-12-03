d1e42ec15c4514359294210d6c083d71df10701e
Nicholas <nc2201@cs.nyu.edu>
2018-12-03 01:46:13 -0500

    Add final evaluate script for producing sentences

---
+++ evaluate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --output=slurm_out/caption_%A_%a.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=80G
+#SBATCH --time=2-00:00:00
+
+ROOT_DIR="/scratch/ovd208/COCO_features/data"
+ARTIFACTS_DIR="$SCRATCH/artifacts/image_captioning"
+NAME='ba_full_test_run'
+RUN_NB="$SLURM_ARRAY_TASK_ID"
+
+BATCH_SIZE=100
+MAX_NB_EPOCHS=40
+
+echo "Starting $NAME"
+
+OPTIONS="--resume_epoch 8"
+
+PYARGS="--root_dir $ROOT_DIR --artifacts_dir $ARTIFACTS_DIR --name $NAME --max_nb_epochs $MAX_NB_EPOCHS --batch_size $BATCH_SIZE --run_nb $RUN_NB $OPTIONS"
+
+source $SCRATCH/pyenv/img_caption/bin/activate
+python src/evaluate_test.py $PYARGS
---
+++ src/evaluate_test.py
@@ -0,0 +1,153 @@
+import os
+import yaml
+import logging
+import pickle
+import argparse
+import matplotlib
+import time
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+import torch
+import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+import utils_experiment as utils
+from data_helpers.data_loader_ks import get_loader
+from data_helpers.vocab import Vocabulary
+from models.caption_model import Caption_Model
+
+#########################################
+#               CONSTANTS               #
+#########################################
+ARGS_FILE = 'args.yml'
+
+#############################
+#           DEVICE          #
+#############################
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+
+def test(args, model, test_loader, len_vocab, beam=None):
+    model.eval()
+    nb_batch = len(test_loader)
+    nb_test = nb_batch * args.batch_size
+    logging.info("Testing {} batches, {} samples.".format(nb_batch, nb_test))
+
+    loss = torch.nn.CrossEntropyLoss()
+    
+    epoch_loss = 0
+
+    with torch.no_grad():
+        for i, (image_ids, features, captions, lengths) in enumerate(test_loader):
+        #print(i)
+            len_captions = len(captions[0])
+            if torch.cuda.is_available():
+                features, captions = features.cuda(), captions.cuda()
+
+            if beam is not None:
+                if (i % (nb_batch//2)) == 0:
+                    sentences = model(features[0:1], 20, beam)
+                    print(sentences)
+            out = model(features, len_captions)
+            n_ex, vocab_len = out.view(-1, len_vocab).shape
+            captions = captions[:,1:]
+            batch_loss = loss(out.view(-1,len_vocab),captions.contiguous().view(1, n_ex).squeeze())
+            epoch_loss+=batch_loss.item()
+   
+    epoch_loss = epoch_loss/nb_batch
+    logging.info("Test loss: " + str(epoch_loss))
+    return epoch_loss
+
+def save_final_captions(args, model, val_loader, max_sent_len, beam_width):
+    nb_batch = len(val_loader)
+    nb_val = nb_batch * args.batch_size
+    assert nb_batch == nb_val # Must be equal for beam search
+    logging.info("Captioning {} batches, {} samples.".format(nb_batch, nb_val))
+
+    s = utils.Sentences(args.experiment_dir)
+    model.eval()
+    with torch.no_grad():
+        for i, (image_ids, features, captions, lengths) in enumerate(val_loader):
+            if torch.cuda.is_available():
+                features = features.cuda()
+            sentence = model(features, max_sent_len, beam_width)
+            s.add_sentence(image_ids[0], sentence[1])
+            if (i % 50) == 0:
+                logging.info("  {:4d}".format(i))
+    logging.info("Saving sentences...")
+    s.save_sentences()
+    logging.info("Done.")
+
+
+def create_model(args, vocab, feature_dim):
+    model = None
+    
+    model = Caption_Model(dict_size=len(vocab),
+                            image_feature_dim=feature_dim, vocab=vocab)
+    
+    if args.resume_epoch > 0:
+        logging.info('Loading checkpoint')
+        args.checkpoint = torch.load(os.path.join(args.experiment_dir, "epoch_" + str(args.resume_epoch) + ".pth.tar" ))
+        model.load_state_dict(args.checkpoint['model_state_dict'])
+        args.current_epoch = args.resume_epoch
+    elif args.resume_epoch < 0:
+        logging.info('Loading best checkpoint')
+        args.checkpoint = torch.load(os.path.join(args.experiment_dir, "best_model" + ".pth.tar" ))
+        model.load_state_dict(args.checkpoint['model_state_dict'])
+        args.current_epoch = args.checkpoint['epoch']
+    else:
+        args.current_epoch = 0
+        logging.info('No checkpoint used')
+    
+    return model
+
+#####################################
+#               MAIN                #
+#####################################
+def main():
+    feature_dim = 2048
+    nb_features = 36
+    args = utils.read_args()
+    args.experiment_dir = os.path.join(args.artifacts_dir,
+                                       args.name,
+                                       str(args.run_nb))
+    if os.path.exists(args.experiment_dir)==False:
+        os.makedirs(args.experiment_dir)
+    utils.initialize_logger(args.experiment_dir)
+    logging.warning("Starting {}, run {}.".format(args.name, args.run_nb))
+
+    args.current_epoch = 0
+    utils.save_args(args, args.experiment_dir, ARGS_FILE)
+
+
+    logging.info("Loading data...")
+    with open(os.path.join(args.root_dir, 'vocab.pkl'), 'rb') as f:
+        vocab = pickle.load(f)
+
+    test_loader = get_loader(data_root=args.root_dir,
+                              vocab=vocab,
+                              batch_size=args.batch_size,
+                              data_type='test',
+                              shuffle=False,
+                              num_workers=0,
+                              debug=False)
+    logging.info("Test loader ready")
+
+    model = create_model(args, vocab, feature_dim)
+    if args.resume_epoch > 0:
+        scheduler.load_state_dict(args.checkpoint['scheduler_state_dict'])
+
+    logging.info(model)
+
+    t0 = time.time()
+    
+    save_final_captions(args, model, test_loader, max_sent_len=12, beam_width=5)
+    test_loss = test(args,model,test_loader, len(vocab), beam=None)
+    logging.info("Testing done in: {:3.1f} seconds".format(time.time() - t0))
+
+if __name__ == "__main__":
+    main()
